84b2fbd3f4e1feba99ba38dbd2b342a94a8be79d
Manal Yassin Hagos <ManalHagos@hotmail.com>
2019-07-31 10:59:55 -0400

    add icon

--- ComputerPurchase/Views/StartForm.Designer.cs
+++ ComputerPurchase/Views/StartForm.Designer.cs
@@ -83,11 +83,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.SavedOrder);
             this.Controls.Add(this.NewOrder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StartForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
